e51534615f23b397cb23ffb1e56cfb4dff4e1c92
Adam Cox <adamcox.dev@gmail.com>
2022-08-19 10:02:29 -0600

    Second commit finsihed sdlc.js questions

--- sdlc.js
+++ sdlc.js
@@ -18,4 +18,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const testingAndIntegration = "Integration testing also known as integration and testing (I&T) is a type of software testing in which the different units, modules or components of a software application are tested as a combined entity. However, these modules may be coded by different programmers."
+const analysisAndRequirements = "Requirement Analysis, also known as Requirement Engineering, is the process of defining user expectations for a new software being built or modified. In software engineering, it is sometimes referred to loosely by names such as requirements gathering or requirements capturing."
+const maintain = "Program maintenance is the process of modifying a software or program after delivery to achieve any of these outcomes − Correct errors. Improve performance. Add functionalities. Remove obsolete portions."
+const design = "Design Code means a code setting out the broad means parameters with reference to which the Developer will secure uniform standards of design quality, character of design, building materials, density of development and site layout within the site. One of the main components of software design is the software requirements analysis (SRA). SRA is a part of the software development process that lists specifications used in software engineering."
+const implementation = "In computer programming, a programming language implementation is a system for executing computer programs. There are two general approaches to programming language implementation:"
