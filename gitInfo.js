821ece19e1a0778fea64e29315189f1c7798015a
Adam Cox <adamcox.dev@gmail.com>
2022-08-19 10:20:43 -0600

    First commit on gitinfo.js

--- gitInfo.js
+++ gitInfo.js
@@ -9,7 +9,8 @@
     It should be a string containing your best definition of what Git is.
 */
 
-//CODE HERE
+const gitDefinition = "Git is an open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency. Git has a tiny footprint with lightning fast performance. It outclasses SCM tools like Subversion, CVS, Perforce, and ClearCase with features like cheap local branching, convenient staging areas, and multiple workflows."
+
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -17,7 +18,7 @@
     It should be a string containing your best definition of what GitHub is.
 */
 
-//CODE HERE
+const gitHubDefinition = "GitHub is a distributed version-control platform where users can collaborate on or adopt open source code projects, fork code, share ideas and more."
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -25,7 +26,7 @@
     It should be a string containing your best definition of what 'git init' does.
 */
 
-//CODE HERE
+const gitInitDefinition = "The git init command creates a new Git repository. It can be used to convert an existing, unversioned project to a Git repository or initialize a new, empty repository. Most other Git commands are not available outside of an initialized repository, so this is usually the first command you'll run in a new project."
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -33,7 +34,7 @@
     It should be a string containing your best definition of what 'git clone' does.
 */
 
-//CODE HERE
+const gitCloneDefinition = "git clone is primarily used to point to an existing repo and make a clone or copy of that repo at in a new directory, at another location. The original repository can be located on the local filesystem or on remote machine accessible supported protocols. The git clone command copies an existing Git repository."
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -41,7 +42,7 @@
     It should be a string containing your best definition of what 'git status' does.
 */
 
-//CODE HERE
+const gitStatusDefinition = "The git status command displays the state of the working directory and the staging area. It lets you see which changes have been staged, which haven't, and which files aren't being tracked by Git. Status output does not show you any information regarding the committed project history."
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -52,8 +53,9 @@
     It should be a string containing the code to add all files.
 */
 
-//CODE HERE
+const gitAddDefinition = "Git add is the first command in a chain of operations that directs Git to save a snapshot of the current project state, into the commit history. When used on its own, git add will promote pending changes from the working directory to the staging area."
 
+const gitAddCode = "git add --all"
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -63,7 +65,7 @@
     It should be a string containing the code to commit using the message "initial commit".
 */
 
-//CODE HERE
+const gitCommitDefinition ""
 
 
 //////////////////PROBLEM 8////////////////////
