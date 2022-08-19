ad6ed2e37f6c0f3fec12fd8c30f0915a2c94656a
Adam Cox <adamcox.dev@gmail.com>
2022-08-19 10:32:30 -0600

    First commit and final commit on variables.js

--- variables.js
+++ variables.js
@@ -1,17 +1,16 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
-
+const myName = "Adam";
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
-//CODE HERE
+const favNum = 15;
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
-//CODE HERE
+let lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -20,11 +19,17 @@
   If it's not, console.log('not lucky 13').
 */
 
-//CODE HERE
+if(favNum === 13){
+  console.log('Lucky 13');
+} else{
+  console.log('Not lucky 13');
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
-//CODE HERE
+for(let i = 0; i < 5; i++){
+  console.log(favNum);
+}
